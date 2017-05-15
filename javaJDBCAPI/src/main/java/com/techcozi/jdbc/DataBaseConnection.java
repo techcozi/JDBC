d42d77f513350e97e4a98d8beaf49f390d7dba31
techcozi <techcoziuser@gmail.com>
2017-05-15 13:20:51 +0530

    Add files via upload

---
+++ javaJDBCAPI/src/main/java/com/techcozi/jdbc/DataBaseConnection.java
@@ -0,0 +1,64 @@
+package com.techcozi.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBaseConnection {
+
+	public static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+	public static final String MYSQL_URL = "jdbc:mysql://localhost/test?"
+											+ "user=root&password=";
+	
+	//public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/mkyongcom","root", "password";
+	
+	private static Connection connection;
+	private static Statement statement;
+	private ResultSet resultSet;
+	private PreparedStatement preparedStatement;
+	
+	public DataBaseConnection(){
+	}
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName(MYSQL_DRIVER);
+			connection = DriverManager.getConnection(MYSQL_URL);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		
+		return connection;
+	}
+	public void readData() throws Exception {
+		try {
+			connection = getConnection(); 
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("select * from test.userbo;");
+			getResultSet(resultSet);
+		}finally{
+			close();
+		}
+	}
+	
+	private void getResultSet(ResultSet resultSet) throws Exception {
+		while(resultSet.next()){
+			 
+			System.out.println(" Record : "+resultSet.getString(2));
+		}
+	}
+	
+	private void close(){
+		try {
+			if(resultSet!=null) resultSet.close();
+			if(statement!=null) statement.close();
+			if(connection!=null) connection.close();
+		} catch(Exception e){}
+	}
+}
---
+++ javaJDBCAPI/src/main/java/com/techcozi/jdbc/TestDataBaseConnection.java
@@ -0,0 +1,9 @@
+package com.techcozi.jdbc;
+
+public class TestDataBaseConnection {
+	
+	public static void main(String[] args) throws Exception {
+		DataBaseConnection dao = new DataBaseConnection();
+		dao.readData();
+	}
+}
